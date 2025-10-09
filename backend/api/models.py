63b5f7da5118350b12eb65af4d4be3123bcfb49f
cleimns <urcleimns@gmail.com>
2025-10-09 21:02:47 +0300

    feat: extended User model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('student', 'Студент'),
+        ('teacher', 'Преподаватель'),
+    ]
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default='student',
+        verbose_name="Роль"
+    )
+    group = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name="Группа"
+    )
+    direction = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        verbose_name="Направление"
+    )
+    
+    
