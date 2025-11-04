1695e83c28c355ed86820a58a6a4cb22a912e367
cleimns <urcleimns@gmail.com>
2025-11-04 18:16:41 +0300

    feat(admin): improve models admin panel

--- backend/schedule_app/admin.py
+++ backend/schedule_app/admin.py
@@ -4,12 +4,23 @@ from .models import Subject, Group, Lesson
 @admin.register(Subject)
 class Subjects(admin.ModelAdmin):
     list_display = ["id", "name", "description"]
+    ordering = ["id"]
+    list_per_page = 30
+    search_fields = ["name"]
+    list_editable = ["name"]
 
 
 @admin.register(Group)
 class Groups(admin.ModelAdmin):
     list_display = ["id", "name", "direction", "description"]
+    ordering = ["id"]
+    search_fields = ["name"]
 
 @admin.register(Lesson)
 class Lessons(admin.ModelAdmin):
     list_display = ["id", "subject", "type", "start_time", "end_time",  "day", "classroom", "teacher", "group", "week_type"]
+    ordering = ["id"]
+    list_per_page = 30
+    search_fields = ["subject"]
+    list_editable = ["type", "week_type"]
+    list_filter = ["subject", "group"]
