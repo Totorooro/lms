75de15bf9d91b775f23e14c01f7c3cdfe3f93463
cleimns <urcleimns@gmail.com>
2025-11-06 19:55:40 +0300

    feat: Add model for events and configure admin panel

---
+++ backend/events_app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Event
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['title', 'start_date', 'location', 'organizer', 'status', 'type_status', 'progress']
+    list_filter = ['status', 'type_status']
+    search_fields = ['title']
+    ordering = ['id']
